5ca134ff2a71eac334de0dc98e025fe30625e207
Abdulrahman Daromosu <73336016+A1bdul@users.noreply.github.com>
2023-03-10 20:06:38 +0100

    Update settings.py

--- Wolmart/settings.py
+++ Wolmart/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = 'Wolmart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.getenv('PGDATABASE')),
+        'USER': str(os.getenv('<PGUSER>')),
+        'PASSWORD': str(os.getenv('<PGPASSWORD>')$,
+        'HOST': str(os.getenv('<PGHOST>')),
+        'PORT': str(os.getenv('<PGPORT>')),
     }
 }
 
